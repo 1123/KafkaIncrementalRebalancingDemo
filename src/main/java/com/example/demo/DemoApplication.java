dceae9ec7d7098fc343da4a4cbbed8c7859b71e4
Benedikt Linse <benedikt@confluent.io>
2021-12-11 21:43:26 +0100

    switching to ConcurrentKafkaListenerContainerFactory

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,12 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 
 @Slf4j
 @EnableKafka
@@ -17,7 +21,7 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@KafkaListener(topics = "test-topic")
+	@KafkaListener(topics = "test-topic", containerFactory = "concurrentKafkaListenerContainerFactory")
 	public void listen(ConsumerRecord<?, ?> cr) {
 		log.info(cr.toString());
 	}
@@ -25,6 +29,15 @@ public class DemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Thread.sleep(10000000);
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory (
+			ConsumerFactory<String , String> consumerFactory, KafkaProperties kafkaProperties) {
+		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory);
+		factory.setConcurrency(5);
+		return factory;
 	}
 }
 
