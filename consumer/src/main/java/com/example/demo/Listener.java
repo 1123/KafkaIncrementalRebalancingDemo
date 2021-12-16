c295a6f712494f6049b24f4139ef17ace3e8d70a
Benedikt Linse <benedikt@confluent.io>
2021-12-16 19:15:55 +0100

    adding consumer delay for simulating a slow consumer

--- consumer/src/main/java/com/example/demo/Listener.java
+++ consumer/src/main/java/com/example/demo/Listener.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +11,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class Listener {
 
+    @Value("${MESSAGE_CONSUME_DELAY_MS}")
+    private long messageConsumeDelay;
+
+    @SneakyThrows
     @KafkaListener(topics = "test-topic")
     public void listen(ConsumerRecord<?, ?> cr) {
+        Thread.sleep(messageConsumeDelay);
         if (cr.offset() % 100 == 0) { log.info(cr.toString()); }
     }
 
