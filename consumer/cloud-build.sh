55ad60996583fec965f58b3a87816fe356f7620d
Benedikt Linse <benedikt@confluent.io>
2021-12-16 13:09:19 +0100

    improving consumer cloudbuild

--- consumer/cloud-build.sh
+++ consumer/cloud-build.sh
@@ -1,2 +1,6 @@
-gcloud builds submit --config cloudbuild.yaml .
+rm -rf cloudbuild
+mkdir -p cloudbuild
+cp target/demo-consumer-0.0.1-SNAPSHOT.jar cloudbuild 
+cp Dockerfile cloudbuild 
+gcloud builds submit --config cloudbuild.yaml cloudbuild
 
